556f3031474edf5354488705106874785a026d36
las2408 <las1@kent.ac.uk>
2026-02-02 13:34:20 +0000

    working camel converter

--- camel.py
+++ camel.py
@@ -1,1 +1,7 @@
-# Enter your code here
+#Camel Case Input
+camelCaseName = input("Enter Variable Name: ")
+for chr in camelCaseName:
+    if chr.isupper():
+        print("_" + chr.lower(), end = "")
+    else:
+        print(chr, end = "")
