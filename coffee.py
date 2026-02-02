0764497a4f6b907ffc47074b1c91ace5965bd6a5
las2408 <las1@kent.ac.uk>
2026-02-02 14:44:09 +0000

    working coffee machine

--- coffee.py
+++ coffee.py
@@ -1,1 +1,20 @@
-#Your code goes here
+#Inputs
+def money():
+    total_due = 75
+    coins = ["5","10","20","50"]
+
+    while total_due > 0:
+         print(f"Amount due: {total_due}")
+         coin = input("Insert coin: ")
+         if coin in coins:
+             total_due -= int(coin)
+         else:
+            print("Invalid Coin")
+
+    if total_due < 0:
+        print(f"Amount owed: {total_due * -1}")
+
+    else:
+        print(f"Total balance = 0")
+
+money()
